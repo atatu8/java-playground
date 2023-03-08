edc83772a147a357b509f4dd5df99bbe650bd647
Atatu8 <slavarevus0@gmail.com>
2023-03-08 19:31:12 +0200

    Додав клас Math

--- src/main/java/atatu/App.java
+++ src/main/java/atatu/App.java
@@ -1,13 +1,25 @@
 package atatu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Math {
+    long addition(long a, long b) {
+        return a + b;
+    }
+
+    public long substraction(long a, long b) {
+        return a - b;
+    }
+
+    public long multiplication(long a, long b) {
+        return a * b;
+    }
+
+    public long devision(long a, long b) {
+        return a / b;
     }
 }
