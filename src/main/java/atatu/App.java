ef7a5ed95a24d14bb09271f0383cfdc32968298b
Atatu8 <slavarevus0@gmail.com>
2023-03-09 18:31:10 +0200

    Перемістив клас Math у власний файл, зробив методи в ньому статичними

--- src/main/java/atatu/App.java
+++ src/main/java/atatu/App.java
@@ -4,22 +4,4 @@ public class App {
     public static void main(String[] args) {
 
     }
-}
-
-class Math {
-    long addition(long a, long b) {
-        return a + b;
-    }
-
-    public long substraction(long a, long b) {
-        return a - b;
-    }
-
-    public long multiplication(long a, long b) {
-        return a * b;
-    }
-
-    public long devision(long a, long b) {
-        return a / b;
-    }
-}
+}   
---
+++ src/main/java/atatu/Math.java
@@ -0,0 +1,17 @@
+public class Math {
+    public static long addition(long a, long b) {
+        return a + b;
+    }
+
+    public static long substraction(long a, long b) {
+        return a - b;
+    }
+
+    public static long multiplication(long a, long b) {
+        return a * b;
+    }
+
+    public static long devision(long a, long b) {
+        return a / b;
+    }
+}
