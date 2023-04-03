95c5867ecb849800b4184c6d848455d53f3abd67
Atatu <slavarevus0@gmail.com>
2023-04-03 11:42:41 +0300

    Переніс задавання значення змінних a, b, result у тести. Перемістив метод ZeroDivisionTest ближче до divisionTest

--- src/test/java/atatu/MathTest.java
+++ src/test/java/atatu/MathTest.java
@@ -4,45 +4,32 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import org.junit.Before;
 
 public class MathTest {
-    long a;
-    long b;
-
-    @Before
-    public void setUp() {
-        a = 10;
-        b = 2;
-    }
-
-    @Test(expected = DivideByZeroException.class)
-    public void zeroDivisionTest() throws DivideByZeroException {
-        Math.division(a, 0);
-    }
-
+    
     @Test
     public void additionTest() {
-        int result = 12;
-        assertEquals("10 + 2 must be equal 12", result, Math.addition(a, b));
+        assertEquals("10 + 2 must be equal 12", 12, Math.addition(10, 2));
     }
-
+    
     @Test
     public void substractionTest() {
-        int result = 8;
-        assertEquals("10 - 2 must be equal 8", result, Math.substraction(a, b));
+        assertEquals("10 - 2 must be equal 8", 8, Math.substraction(10, 2));
     }
 
     @Test
     public void multiplicationTest() {
-        int result = 20;
-        assertEquals("10 * 2 must be equal 20", result, Math.multiplication(a, b));
+        assertEquals("10 * 2 must be equal 20", 20, Math.multiplication(10, 2));
+    }
+    
+    @Test(expected = DivideByZeroException.class)
+    public void zeroDivisionTest() throws DivideByZeroException {
+        Math.division(10, 0);
     }
 
     @Test
     public void divisionTest() throws DivideByZeroException {
-        int result = 5;
-        assertEquals("10 / 2 must be equal 5", result, Math.division(a, b));
+        assertEquals("10 / 2 must be equal 5", 5, Math.division(10, 2));
     }
 
 }
